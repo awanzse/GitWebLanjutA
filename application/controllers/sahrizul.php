cb1fd6c9119b819a653b928c9e1fffaba093d7bc
Sahrizul <sahrizulrangbonuo@gmail.com>
2019-05-15 15:46:58 +0700

    Update sahrizul.php

--- application/controllers/sahrizul.php
+++ application/controllers/sahrizul.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Indra extends CI_Controller {
+class Sahrizul extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
