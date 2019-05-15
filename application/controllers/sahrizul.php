c747f9c2bd6fe5336d03cd5eb5b69ee93f654b61
Sahrizul <sahrizulrangbonuo@gmail.com>
2019-05-15 15:48:08 +0700

    Revert "Update sahrizul.php"
    This reverts commit cb1fd6c9119b819a653b928c9e1fffaba093d7bc.

--- application/controllers/sahrizul.php
+++ application/controllers/sahrizul.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Sahrizul extends CI_Controller {
+class Indra extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
