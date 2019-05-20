66a5f26edc4d023c586e4cf134927d2a9f753042
kurniawanrudi31 <50655281+kurniawanrudi31@users.noreply.github.com>
2019-05-20 13:20:56 +0800

    Create profil.php

---
+++ application/views/profil.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" href="../../../tugas/style.css" type="text/css">
+</head>
+<body>
+<div id="wrapper">  
+ <div id="container">      
+  <header></header> <!--HTML5 -->    
+     <nav>
+     <ul>
+     <li><a href="../../../tugas/index.html">HOME</a></li>   
+     <li><a href="#">TUTORIAL</a>                   
+     <ul class="submenu">                           
+     <li><a href="../../../tugas/airterjun.html">Affter effect</a></li>                           
+     <li><a href="#">Photo Shop</a></li>
+     <li><a href="#">Dll</a></li>      
+     </ul>
+     </li>
+     <li><a href="../../../tugas/video.html">VIDEO</a></li>
+     <li><a href="../../../tugas/profil.html">PROFIL SAYA</a></li>
+     <li><a href="../../../tugas/kontak.html">KONTAK</a></li>
+     </ul>
+     </nav>   <!--HTML5 -->         
+      <div id="content">
+      <div id="single">
+      <div id="thumbnail-image" style="background:url(../../../tugas/images/lanang.jpg)">&nbsp;</div>
+      </div>
+      </div>
+      <div id="sidebar">
+      <li class="widget"> 
+      <td><h2>Biodata Saya</h2></td>
+      <ul>
+      <td>NAMA</td>
+      <td>:</td>
+      <td>RUDI KURNIAWAN</td>
+      </ul>
+      <ul>
+      <td>PANGGILAN</td>
+      <td>:</td>
+      <td>RudiKa31</td>
+      </ul>
+      <ul>
+      <td>TTL</td>
+      <td>:</td>
+      <td>Pasir Pengaraian 22 November 1996</td>
+      </ul>
+      <ul>
+      <td>ALAMAT</td>
+      <td>:</td>
+      <td>Pasir Pengaraian,Dusun Sukajadi </td>
+       <li class="widget">
+      <h2>Music</h2>
+      <audio id="audio" src="sounds//Maroon_5_One_More_Night.mp3"controls></audio>
+      </li>        
+      </ul>
+      </div>
+     <video id="video" width="600" height="400"src="../../../tugas/movies/Proyek16_2018_07_05_1.mp4" controls></video>
+       
+      <footer>   <!--HTML5 -->           
+      <div id="column">
+      <h2>Dapatkan Bantuan Dan Dukungan</h2>
+      <div id="column2">tanyakan hal yang ingin anda tanyakan bila anda tidak mengerti...</div>
+      </div>           
+      <div id="column">
+      <h2>Hubungi Kami</h2>
+      <p>Rudikurniawan310597@gmail.com</p>
+      <p>081378033544 </p>
+      </div>           
+      <div id="column">
+      <h2>Temukan Kami di ...</h2>  
+      <a href="http://facebook.com/directnewsaccess">Facebook</a><br>  
+      <a href="http://Whatsapp.com/directnewsaccess">whatsapp</a><br>
+      </div>          
+      </footer>  
+      </div>     
+      </div> 
+    </footer>  
+    </div>     
+    </div>
+</body>
+</html>
