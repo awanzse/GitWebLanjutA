32e93803f708228c1d7bd93752f5a377fa39b65e
fitriani1110 <50658617+fitriani1110@users.noreply.github.com>
2019-05-21 08:29:03 -0700

    Fitriani

---
+++ application/views/fitriani.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.EF {
+	font-size: 60px;
+}
+.ASF {
+	font-size: 50px;
+}
+.E {
+	font-size: 60px;
+}
+.E p {
+	font-size: 70px;
+}
+DW {
+	font-size: 70px;
+}
+body p {
+	font-size: 80px;
+}
+body p {
+	font-size: 60px;
+}
+</style>
+</head>
+
+<body>
+<table width="3136" height="1457" border="10">
+  <tr>
+    <td width="1153" rowspan="3" bgcolor="#FF3366"><div align="center"><img src="<?= base_url() ?>asset/Fitri.jpg" alt="wf" width="800" height="1200" align="middle" /></div></td>
+    <td width="1949" height="133" bgcolor="#0099CC" class="EF"><div align="center">BIODATA FITRIANI</div></td>
+  </tr>
+  <tr>
+    <td height="106" bgcolor="#FF3366" class="ASF"><marquee>UNIVERSITAS PASIR PENGARAIAN</marquee></td>
+  </tr>
+  <tr>
+    <td height="1190" bgcolor="#0099CC"><p align="center">NAMA : FITRIANI</p>
+      <p align="center">PRODI : TEKNIK INFORMATIKA</p>
+    <p align="center">NIM : 1637018</p>
+    <p align="center">AGAMA : ISLAM</p>
+    <p align="center">TTL : MAHATO, 11 OKTOBER 1997</p>
+    <p align="center">TINGGI : 153 CM</p>
+    <p align="center">BERAT : 40 KG</p>
+    <p align="center">STATUS : MAHASISWA</p>
+    <p align="center">HOBI : TRAVELING DAN SHOPING</p>
+    <p align="center">&nbsp;</p>
+    <p align="center">&quot;Jangan Menyerah Selagi Bisa Berusaha,</p>
+    <p align="center">Usaha itu Bukan Cuma Gagal Terus Menyerah,berusaha lah sampai</p>
+    <p align="center">titik darah penghabisan&quot;</p></td>
+  </tr>
+</table>
+</body>
+</html>
