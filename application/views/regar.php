c961e22c0839fd5d5b6c8ad83685035dddcd29ac
efrida212 <50658310+efrida212@users.noreply.github.com>
2019-05-21 05:55:50 -0700

    Web Efrida

---
+++ application/views/regar.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.S {
+	font-size: 60px;
+	font-family: "Comic Sans MS", cursive;
+}
+.S {
+	color: #909;
+	font-family: "Comic Sans MS", cursive;
+}
+.d {
+	font-size: 24px;
+}
+.d {
+	font-size: 24px;
+}
+.f {
+	font-size: 24px;
+}
+</style>
+</head>
+
+<body>
+<table width="1774" height="204" border="1">
+  <tr>
+    <td width="399"><img src="<?= base_url() ?>asset/Efrida.jpg" width="399" height="500" alt="d" /></td>
+    <td width="1359" bgcolor="#00FF00"><div align="center" class="S">BIODATA EFRIDA REGAR</div>
+    <span class="S"></span></td>
+  </tr>
+</table>
+<p><marquee>BIODATA EFRIDA REGAR</marquee></p>
+<table width="1776" height="430" border="1">
+  <tr>
+    <td width="137" bgcolor="#FF0000" class="d">Nama</td>
+    <td width="1603" bgcolor="#FF0000" class="f">Efrida Regar</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">TTL</td>
+    <td bgcolor="#FF0000" class="f">Selewek, 12 Oktober 1997</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Alamat</td>
+    <td bgcolor="#FF0000" class="f">Selewek</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Jenis Kelamin</td>
+    <td bgcolor="#FF0000" class="f">Perempuan</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Riwayat Pendidikan</td>
+    <td bgcolor="#FF0000" class="f"><p>SDN 005 Tambusai Timur</p>
+    <p>SMPN 005 Bukit Senyum</p>
+    <p>SMKN 1 Barumun</p>
+    <p>Universitas Pasir Pengarauan</p></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Fakultas </td>
+    <td bgcolor="#FF0000" class="f">Ilmu Komputer</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Program Studi</td>
+    <td bgcolor="#FF0000" class="f">Teknik Informatika</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Agama</td>
+    <td bgcolor="#FF0000" class="f">Islam</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Hobi</td>
+    <td bgcolor="#FF0000" class="f">Traveling</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Cita-Cita</td>
+    <td bgcolor="#FF0000" class="f">Pengusaha</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p><em></em></p>
+</body>
+</html>
