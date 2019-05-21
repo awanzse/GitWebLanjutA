f0be15e95b020b3a72ae89ce92977fd3ed397ad5
Monaliisa07 <50397142+Monaliisa07@users.noreply.github.com>
2019-05-21 07:39:31 -0700

    monalisa

---
+++ application/views/lisa.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WEB KU</title>
+<style type="text/css">
+.n {
+	font-size: 36px;
+}
+.k {
+	font-size: 24px;
+}
+.l {
+	font-size: 24px;
+}
+</style>
+</head>
+
+<body bgcolor="#CCCCCC">
+<table width="745" height="108" border="0">
+  <tr>
+    <td bgcolor="#00FF33"><div align="center" class="n">SELAMAT DATANG DI WEBSITE KU</div></td>
+  </tr>
+</table>
+<p class="k"><marquee>Biodata diri</marquee></p>
+<table width="745" border="0">
+  <tr>
+    <td width="335" bgcolor="#FF99FF">Nama</td>
+    <td width="11" bgcolor="#FF99FF">:</td>
+    <td width="385" bgcolor="#FF99FF">Mona</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">NIM</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">1637020</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Alamat</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">Tanjung Belit</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Agama</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">Islam</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Hobi</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">Masak</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Cita - cita</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">Guru</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Instagram</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">@Monalisaa07</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Facebook</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">Monalisa</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF99FF">Contact Person</td>
+    <td bgcolor="#FF99FF">:</td>
+    <td bgcolor="#FF99FF">085263959921</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p class="l"><marquee>Koleksi Foto</marquee></p>
+<p class="l"><img src="<?= base_url() ?>asset/Mona1.jpg" width="455" height="604"> </p>
+<p class="l"><img src="<?= base_url() ?>asset/Mona2.jpg" width="460" height="704"></p>
+<p class="l">&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
