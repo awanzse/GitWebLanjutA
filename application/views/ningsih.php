c0b4df5892d619d6e07128103339a86b2b8a9970
SilviaNingsih14 <50655251+SilviaNingsih14@users.noreply.github.com>
2019-05-21 07:03:55 -0700

    Silvia Ningsih

---
+++ application/views/ningsih.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.k {
+	font-size: 24px;
+}
+</style>
+</head>
+
+<body>
+<table width="502" height="491" border="2">
+  <tr>
+    <td colspan="2" bgcolor="#FF3333">  <div align="center"><img src="<?= base_url() ?>asset/Silvia.jpg" alt="s" width="350" height="500" align="texttop" /></div></td>
+  </tr>
+  <tr>
+    <td colspan="2" bgcolor="#993333"><div align="center"><span class="k"><marquee>BIODATA SILVIA NINGSIH</marquee></span></div></td>
+  </tr>
+  <tr>
+    <td width="204" bgcolor="#0066FF"><span class="k">Nama</span></td>
+    <td width="264" bgcolor="#0066FF"><span class="k">Silvia Ningsih</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">TTL</span></td>
+    <td bgcolor="#0066FF"><span class="k">Kampung Baru, 13 Agustus 1997</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Alamat</span></td>
+    <td bgcolor="#0066FF"><span class="k">Pekan Tebih</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Agama</span></td>
+    <td bgcolor="#0066FF"><span class="k">Islam</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Jenis Kelamin </span></td>
+    <td bgcolor="#0066FF"><span class="k">Perempuan</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Hobi</span></td>
+    <td bgcolor="#0066FF"><span class="k">Membaca</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Cita-cita</span></td>
+    <td bgcolor="#0066FF"><span class="k">Dosen</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Status</span></td>
+    <td bgcolor="#0066FF"><span class="k">Mahasiswa</span></td>
+  </tr>
+</table>
+</body>
+</html>
