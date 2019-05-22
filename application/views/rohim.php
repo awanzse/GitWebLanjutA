de025af91d8fcf3df72a3a4177ba5de431d0ebc9
selametrohim <selametrohim209@gmail.com>
2019-05-22 23:59:41 +0700

    Update rohim.php

--- application/views/rohim.php
+++ application/views/rohim.php
@@ -2,103 +2,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Selamet Rohim</title>
-<style type="text/css">
-<<<<<<< HEAD
-.WD {
-	font-size: 24px;
-	font-family: "Comic Sans MS", "OCR A Std";
-}
-.S {
-	font-family: "Comic Sans MS", "OCR A Std";
-}
-.S {
-	font-size: 80px;
-	font-family: "Comic Sans MS", "OCR A Std";
-}
-.S tr td .WD strong {
-	font-size: 36px;
-	font-family: "Comic Sans MS", "OCR A Std";
-}
-.d {
-	color: #33F;
-=======
-.S {
-	font-size: 60px;
-	font-family: "Comic Sans MS", cursive;
-}
-.S {
-	color: #909;
-	font-family: "Comic Sans MS", cursive;
-}
-.d {
-	font-size: 24px;
-}
-.d {
-	font-size: 24px;
-}
-.f {
-	font-size: 24px;
->>>>>>> fadd846c3312aacf9c42d967e42bf5471dbf1d06
-}
-</style>
+<title>Untitled Document</title>
 </head>
+
 <body>
-<table width="1774" height="204" border="1">
+<table width="1001" height="183" border="1">
   <tr>
-    <td width="399"><img src="<?= base_url() ?>asset/Au.jpg" width="399" height="500" alt="d" /></td>
-    <td width="1359" bgcolor="#00FFFF"><div align="center" class="S">BIODATA KU</div>
-    <span class="S"></span></td>
+    <th scope="col"><img src="header.JPG" width="1285" height="180" /></th>
   </tr>
 </table>
-<p><marquee> WELCOME TO MY WEB </marquee></p>
-<table width="1776" height="430" border="1">
+<table width="1294" border="1">
   <tr>
-    <td width="137" bgcolor="#00FF00" class="d">Nama</td>
-    <td width="1603" bgcolor="#00FF00" class="f">Selamet Rohim</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">TTL</td>
-    <td bgcolor="#00FF00" class="f">ROKAN, 04-07-1998</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Alamat</td>
-    <td bgcolor="#00FF00" class="f">Muara Katogan</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Jenis Kelamin</td>
-    <td bgcolor="#00FF00" class="f">LAKI-LAKI</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Riwayat Pendidikan</td>
-    <td bgcolor="#00FF00" class="f"><p>SDN 004 Bangun Purba</p>
-    <p>SMP Negeri 1 Bangun Purba</p>
-    <p>SMA Negeri 1 Bangun Purba</p>
-    <p>Universitas Pasir Pengarauan</p></td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Fakultas </td>
-    <td bgcolor="#00FF00" class="f">Ilmu Komputer</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Program Studi</td>
-    <td bgcolor="#00FF00" class="f">Teknik Informatika</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Agama</td>
-    <td bgcolor="#00FF00" class="f">100 % Islam</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Hobi</td>
-    <td bgcolor="#00FF00" class="f">Sepak Bola</td>
-  </tr>
-  <tr>
-    <td bgcolor="#00FF00" class="d">Cita-Cita</td>
-    <td bgcolor="#00FF00" class="f">Pengusaha Sukses</td>
+    <marque> <th bgcolor="#0000FF" scope="col">WELCOME TO MY WEB</th> mar
   </tr>
 </table>
+<p><strong>Profil</strong></p>
+<table width="200" height="169" border="1">
+  <tr>
+    <th bgcolor="#3333FF" scope="col"><img src="../../Tugas Web/Au.jpg" width="169" height="132" /></th>
+  </tr>
+</table>
+<p>Nama Lengkap : Selamet Rohim</p>
+<p>TTL	  : Muara Katogan, 29 September 1997</p>
+<p>Alamat : Muara Katogan</p>
+<p>Agama : 100 % Islam</p>
 <p>&nbsp;</p>
-<p><em></em></p>
->>>>>>> fadd846c3312aacf9c42d967e42bf5471dbf1d06
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 </body>
 </html>
