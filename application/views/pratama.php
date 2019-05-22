4f4c4e299f0e76b5d1fb0ab151bfdde4ff6d8100
jerryrokan <jerrypratama.rkr@gmail.com>
2019-05-22 15:48:10 +0700

    jerrykho
    assalamualaikum

---
+++ application/views/pratama.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.S {
+	font-size: 60px;
+	font-family: "Comic Sans MS", cursive;
+}
+.S {
+	color: #909;
+	font-family: "Comic Sans MS", cursive;
+}
+.d {
+	font-size: 24px;
+}
+.d {
+	font-size: 24px;
+}
+.f {
+	font-size: 24px;
+}
+</style>
+</head>
+
+<body>
+<table width="1774" height="204" border="1">
+  <tr>
+    <td width="399"><img src="<?= base_url() ?>asset/IMG_1879.jpg" width="399" height="500" alt="d" /></td>
+    <td width="1359" bgcolor="#00FFFF"><div align="center" class="S">BIODATA JERRY PRATAMA</div>
+    <span class="S"></span></td>
+  </tr>
+</table>
+<p><marquee>JERRY PRATAMA ROKAN </marquee></p>
+<table width="1776" height="430" border="1">
+  <tr>
+    <td width="137" bgcolor="#FF0000" class="d">Nama</td>
+    <td width="1603" bgcolor="#FF0000" class="f">JERRY PRATAMA</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">TTL</td>
+    <td bgcolor="#FF0000" class="f">ROKAN, 04-07-1998</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Alamat</td>
+    <td bgcolor="#FF0000" class="f">ROKAN KOTO RUANG</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Jenis Kelamin</td>
+    <td bgcolor="#FF0000" class="f">LAKI-LAKI</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Riwayat Pendidikan</td>
+    <td bgcolor="#FF0000" class="f"><p>SDN 001 ROKAN IV KOTO</p>
+    <p>SMPN 001 ROKAN IV KOTO</p>
+    <p>SMAN 001 ROKAN IV KOTO</p>
+    <p>Universitas Pasir Pengarauan</p></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Fakultas </td>
+    <td bgcolor="#FF0000" class="f">Ilmu Komputer</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Program Studi</td>
+    <td bgcolor="#FF0000" class="f">Teknik Informatika</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Agama</td>
+    <td bgcolor="#FF0000" class="f">Islam</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Hobi</td>
+    <td bgcolor="#FF0000" class="f">BERDOA SUPAYA MASUK SURGA</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000" class="d">Cita-Cita</td>
+    <td bgcolor="#FF0000" class="f">MEMBAHAGIAKAN ORANG-ORANG YANG KURANG MAMPU</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p><em></em></p>
+</body>
+</html>
