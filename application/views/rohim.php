c9da2b0d93d9ab54ca1bf8e2a8e380d537ab2fee
jerryrokan <jerrypratama.rkr@gmail.com>
2019-05-22 18:13:40 +0700

    Update rohim.php

--- application/views/rohim.php
+++ application/views/rohim.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Selamet Rohim</title>
 <style type="text/css">
 <<<<<<< HEAD
 .WD {
@@ -43,66 +43,7 @@
 }
 </style>
 </head>
-
 <body>
-<<<<<<< HEAD
-<table width="821" border="0" class="S">
-  <tr>
-    <td width="815" height="86" bgcolor="#FF8080"><div align="center" class="WD"><strong>PROFIL Selamet Rohim</strong></div></td>
-  </tr>
-</table>
-<table width="821" border="0">
-  <tr>
-    <td class="d"><marquee>Univeristas Pasir Pengraian - Teknik Informatika</marquee></td>
-  </tr>
-</table>
-<img src="<?= base_url() ?>asset/" width="820" height="550" alt="ok" />
-<table width="820" height="399" border="0">
-  <tr>
-    <td width="238" bgcolor="#FF0000">Nama Lengkap</td>
-    <td width="548" bgcolor="#0000FF">Selamet Rohim</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Nama Panggilan</td>
-    <td bgcolor="#0000FF">Rohim</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">TTL</td>
-    <td bgcolor="#0000FF">Muara Katogan, 29 - 09 - 1997</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Agama</td>
-    <td bgcolor="#0000FF">100 % Islam</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Jenis Kelamin</td>
-    <td bgcolor="#0000FF">Laki - Laki</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Alamat</td>
-    <td bgcolor="#0000FF">Muara Katogan</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Tinggi/ Berat Badan</td>
-    <td bgcolor="#0000FF">167 cm/ 54 Kg</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Hoby</td>
-    <td bgcolor="#0000FF">Sepak Bola</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Riwayat Pendidikan</td>
-    <td bgcolor="#0000FF"><p>1. SDN 004 Bangun Purba</p>
-    <p>2. SMP Negeri 3 Bangun Purba</p>
-    <p>3. SMA Negeri 1 Bangun Purba</p>
-    <p>4. UNIVERSITAS PASIR PENGARAIAN</p></td>
-  </tr>
-</table>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p></p>
-<p class="S">&nbsp;</p>	
-=======
 <table width="1774" height="204" border="1">
   <tr>
     <td width="399"><img src="<?= base_url() ?>asset/Au.jpg" width="399" height="500" alt="d" /></td>
