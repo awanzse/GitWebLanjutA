89e46332c35fa1d474786059c8fd414fd9c43abf
selametrohim <selametrohim209@gmail.com>
2019-05-22 17:24:27 +0700

    selametrohim
    UPDATE

--- application/views/rohim.php
+++ application/views/rohim.php
@@ -48,7 +48,7 @@
 <<<<<<< HEAD
 <table width="821" border="0" class="S">
   <tr>
-    <td width="815" height="86" bgcolor="#FF8080"><div align="center" class="WD"><strong>PROFIL JERRY PRATAMA</strong></div></td>
+    <td width="815" height="86" bgcolor="#FF8080"><div align="center" class="WD"><strong>PROFIL Selamet Rohim</strong></div></td>
   </tr>
 </table>
 <table width="821" border="0">
@@ -60,19 +60,19 @@
 <table width="820" height="399" border="0">
   <tr>
     <td width="238" bgcolor="#FF0000">Nama Lengkap</td>
-    <td width="548" bgcolor="#0000FF">JERRY PRATAMA</td>
+    <td width="548" bgcolor="#0000FF">Selamet Rohim</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Nama Panggilan</td>
-    <td bgcolor="#0000FF">JERRY</td>
+    <td bgcolor="#0000FF">Rohim</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">TTL</td>
-    <td bgcolor="#0000FF">ROKAN, 04-07-98</td>
+    <td bgcolor="#0000FF">Muara Katogan, 29 - 09 - 1997</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Agama</td>
-    <td bgcolor="#0000FF">Islam</td>
+    <td bgcolor="#0000FF">100 % Islam</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Jenis Kelamin</td>
@@ -80,22 +80,22 @@
   </tr>
   <tr>
     <td bgcolor="#FF0000">Alamat</td>
-    <td bgcolor="#0000FF">ROKAN KOTO RUANG</td>
+    <td bgcolor="#0000FF">Muara Katogan</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Tinggi/ Berat Badan</td>
-    <td bgcolor="#0000FF">168 cm/ 45 Kg</td>
+    <td bgcolor="#0000FF">167 cm/ 54 Kg</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Hoby</td>
-    <td bgcolor="#0000FF">TAARUF</td>
+    <td bgcolor="#0000FF">Sepak Bola</td>
   </tr>
   <tr>
     <td bgcolor="#FF0000">Riwayat Pendidikan</td>
-    <td bgcolor="#0000FF"><p>1. SDN 001 ROKAN</p>
-    <p>2. SMPN 1 ROKAN</p>
-    <p>3. SMAN 1 ROKAN</p>
-    <p>4. UPP</p></td>
+    <td bgcolor="#0000FF"><p>1. SDN 004 Bangun Purba</p>
+    <p>2. SMP Negeri 3 Bangun Purba</p>
+    <p>3. SMA Negeri 1 Bangun Purba</p>
+    <p>4. UNIVERSITAS PASIR PENGARAIAN</p></td>
   </tr>
 </table>
 <p>&nbsp;</p>
@@ -110,7 +110,7 @@
     <span class="S"></span></td>
   </tr>
 </table>
-<p><marquee>WELCOME TO MY WEB</marquee></p>
+<p><marquee> WELCOME TO MY WEB </marquee></p>
 <table width="1776" height="430" border="1">
   <tr>
     <td width="137" bgcolor="#00FF00" class="d">Nama</td>
@@ -118,20 +118,20 @@
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">TTL</td>
-    <td bgcolor="#00FF00" class="f">Muara Katogan, 29-09-1997</td>
+    <td bgcolor="#00FF00" class="f">ROKAN, 04-07-1998</td>
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">Alamat</td>
-    <td bgcolor="#00FF00" class="f">Muara Katogan. RT 02, RW. 06</td>
+    <td bgcolor="#00FF00" class="f">Muara Katogan</td>
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">Jenis Kelamin</td>
-    <td bgcolor="#00FF00" class="f">Laki - Laki</td>
+    <td bgcolor="#00FF00" class="f">LAKI-LAKI</td>
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">Riwayat Pendidikan</td>
     <td bgcolor="#00FF00" class="f"><p>SDN 004 Bangun Purba</p>
-    <p>SMP Negeri 3 Bangun Purba</p>
+    <p>SMP Negeri 1 Bangun Purba</p>
     <p>SMA Negeri 1 Bangun Purba</p>
     <p>Universitas Pasir Pengarauan</p></td>
   </tr>
@@ -149,7 +149,7 @@
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">Hobi</td>
-    <td bgcolor="#00FF00" class="f">Sepak bola</td>
+    <td bgcolor="#00FF00" class="f">Sepak Bola</td>
   </tr>
   <tr>
     <td bgcolor="#00FF00" class="d">Cita-Cita</td>
