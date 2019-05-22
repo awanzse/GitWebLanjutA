eaa1851df98a24a0a535c19cfdcb99367f9c9f79
jerryrokan <jerrypratama.rkr@gmail.com>
2019-05-22 18:03:47 +0700

    Update pratama.php

--- application/views/pratama.php
+++ application/views/pratama.php
@@ -45,64 +45,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
-<table width="821" border="0" class="S">
-  <tr>
-    <td width="238" height="86" bgcolor="#FF8080"><div align="center" class="WD"><strong>PROFIL JERRY PRATAMA</strong></div></td>
-  </tr>
-</table>
-<table width="821" border="0">
-  <tr>
-    <td class="d"><marquee>Univeristas Pasir Pengraian - Teknik Informatika</marquee></td>
-  </tr>
-</table>
-<img src="<?= base_url() ?>asset/" width="820" height="550" alt="ok" />
-<table width="820" height="399" border="0">
-  <tr>
-    <td width="238" bgcolor="#FF0000">Nama Lengkap</td>
-    <td width="548" bgcolor="#0000FF">JERRY PRATAMA</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Nama Panggilan</td>
-    <td bgcolor="#0000FF">JERRY</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">TTL</td>
-    <td bgcolor="#0000FF">ROKAN, 04-07-98</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Agama</td>
-    <td bgcolor="#0000FF">Islam</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Jenis Kelamin</td>
-    <td bgcolor="#0000FF">Laki - Laki</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Alamat</td>
-    <td bgcolor="#0000FF">ROKAN KOTO RUANG</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Tinggi/ Berat Badan</td>
-    <td bgcolor="#0000FF">168 cm/ 45 Kg</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Hoby</td>
-    <td bgcolor="#0000FF">TAARUF</td>
-  </tr>
-  <tr>
-    <td bgcolor="#FF0000">Riwayat Pendidikan</td>
-    <td bgcolor="#0000FF"><p>1. SDN 001 ROKAN</p>
-    <p>2. SMPN 1 ROKAN</p>
-    <p>3. SMAN 1 ROKAN</p>
-    <p>4. UPP</p></td>
-  </tr>
-</table>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p></p>
-<p class="S">&nbsp;</p>	
-=======
 <table width="1774" height="204" border="1">
   <tr>
     <td width="399"><img src="<?= base_url() ?>asset/IMG_1879.jpg" width="399" height="500" alt="d" /></td>
