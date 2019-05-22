bbb822fcea0f4ea5e3ae713b147616f2b9d0b208
ahirudin <ahirudin0405@gmail.com>
2019-05-22 17:42:12 +0700

    ahirudin

---
+++ application/views/zein.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.k {
+	font-size: 24px;
+}
+</style>
+</head>
+
+<body>
+<table width="502" height="491" border="2">
+  <tr>
+    <td colspan="2" bgcolor="#FF3333">  <div align="center"><img src="<?= base_url() ?>asset/zein.jpg" alt="s" width="350" height="500" align="texttop" /></div></td>
+  </tr>
+  <tr>
+    <td colspan="2" bgcolor="#993333"><div align="center"><span class="k"><marquee>BIODATA ahirruddin</marquee></span></div></td>
+  </tr>
+  <tr>
+    <td width="204" bgcolor="#0066FF"><span class="k">Nama</span></td>
+    <td width="264" bgcolor="#0066FF"><span class="k">ahirruddin</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">TTL</span></td>
+    <td bgcolor="#0066FF"><span class="k">bagan batu 5 april 1996</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Alamat</span></td>
+    <td bgcolor="#0066FF"><span class="k">desa batang kumu</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Agama</span></td>
+    <td bgcolor="#0066FF"><span class="k">Islam</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Jenis Kelamin </span></td>
+    <td bgcolor="#0066FF"><span class="k">lelaki</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Hobi</span></td>
+    <td bgcolor="#0066FF"><span class="k">sholat</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Cita-cita</span></td>
+    <td bgcolor="#0066FF"><span class="k">pramugara</span></td>
+  </tr>
+  <tr>
+    <td bgcolor="#0066FF"><span class="k">Status</span></td>
+    <td bgcolor="#0066FF"><span class="k">Mahasiswa</span></td>
+  </tr>
+</table>
+</body>
+</html>
