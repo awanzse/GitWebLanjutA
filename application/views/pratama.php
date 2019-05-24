60fcf589b7da8a558c751b251e9d03502e0be4aa
jerryrokan <jerrypratama.rkr@gmail.com>
2019-05-24 14:46:01 +0700

    jerry

--- application/views/pratama.php
+++ application/views/pratama.php
@@ -1,36 +1,76 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-<style type="text/css">
-<<<<<<< HEAD
-</style>
+<meta charset="utf-8">
+<title>Jerry Pratama</title>
+<link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css" type="text/css"> 
 </head>
-
 <body>
-<table width="1774" height="204" border="1">
-  <tr>
-    <td width="399"><img src="<?= base_url() ?>asset/IMG_1879.jpg" width="399" height="500" alt="d" /></td>
-    <td width="1659"bgcolor="#00FFFF"><div align="center" class="S">BIODATA JERRY PRATAMA</div>
-    <span class="S"></span></td>
-  </tr>
-</table>
-<p><marquee>SELAMAT DATANG DI WEB SAYA </marquee></p>
-<table width="1776" height="430" border="1">
-  <tr>
-    <tr>
-    <td height="1190" bgcolor="#00FFFF"><p align="center">NAMA : JERRY PRATAMA</p>
-      <p align="center">PRODI : TEKNIK INFORMATIKA</p>
-    <p align="center">NIM : 1637012</p>
-    <p align="center">AGAMA : ISLAM</p>
-    <p align="center">TTL : ROKAN, 04 JULY 1998</p>
-    <p align="center">TINGGI : 158 CM</p>
-    <p align="center">BERAT : 44 KG</p>
-    <p align="center">STATUS : MAHASISWA</p>
-    <p align="center">HOBI : BERDOA</p>
-    <p align="center">&nbsp;</p>
-  </tr>
-</table>
+ <div id="wrapper">  
+ <div id="container">      
+  <header></header>    
+    <nav><marquee>SELAMAT DATANG DI WEB JERRY PRATAMA</marquee>
+    </nav>
+<li>   
+</li> 
+     </nav>            
+    <div id="content">
+    <div id="single">    
+    <h2>BIODATA SAYA</h2>
+    <table width="600" height="314" border="0">
+      <tr>
+        <td width="228" rowspan="8"><img src="<?= base_url() ?>/assets/IMG-20180304-WA0035.jpg" width="217" height="202"></td>
+        <td width="148">Nama</td>
+        <td width="10">:</td>
+        <td width="225">Jerry Pratama</td>
+      </tr>
+      <tr>
+        <td>NIM</td>
+        <td>:</td>
+        <td>1637012</td>
+      </tr>
+      <tr>
+        <td>Tempat, Tanggal Lahir</td>
+        <td>:</td>
+        <td>Rokan, 04 Juli 1998</td>
+      </tr>
+      <tr>
+        <td>Alamat</td>
+        <td>:</td>
+        <td>Rokan Koto Ruang, Kec. Rokan IV koto</td>
+      </tr>
+      <tr>
+        <td>Hobi</td>
+        <td>:</td>
+        <td>Olahraga</td>
+      </tr>
+      <tr>
+        <td>Riwayat Pendidikan</td>
+        <td>:</td>
+        <td><p>1. SDN 1 ROKAN IV KOTO</p>
+        <p>2. SMPN 1 ROKAN IV KOTO</p>
+        <p>3. SMAN 1 ROKAN IV KOTO</p>
+        <p>4. Universitas Pasir Pengaraian</p></td>
+      </tr>
+      <tr>
+        <td>Prodi</td>
+        <td>:</td>
+        <td>Teknik Informatika</td>
+      </tr>
+      <tr>
+        <td>Fakultas</td>
+        <td>:</td>
+        <td>Fakultas Ilmu Komputer</td>
+      </tr>
+    </table>
+    <p>&nbsp;</p> 
+    </div>
+<footer>          
+    <div id="column">  
+    <h2>Motivasi Hidup</h2>      
+      BERJUANG SAMPAI TITIK PENGHABISAN!!!!!!!!!!<br>
+     
+</footer> 
+</div>     
 </body>
 </html>
